827de96012f0f5e1a5aebb6a22629484804d626c
Yu-peng Fu <yfu@z4r8p7.42lyon.fr>
2020-12-14 12:12:29 +0100

    for testing map

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ typedef t_list* node;
 void *f(void *a){
 	unsigned char * c;;
 	c = (unsigned char*) a;
-	*c *= 2;
+	c[0]++;
 	return c;
 }
 
@@ -17,17 +17,23 @@ int main(){
 	//test sort
 	//test lstmap
 	node head, temp;
-	int val = 0;
+	char	*val;
+	val = (char*)malloc(2 * sizeof(char));
+	val[0] = 'a';
+	val[1] = 0;
 	head = ft_lstnew((void*)val++);
 	int	i = 0;
 	while (i++ < 10){
-		temp = ft_lstnew((void*)val++);
+		char	*new;
+		new = (char*)malloc(2 * sizeof(char));
+		new[0] = 'a' + i;
+		new[1] = '\0';
+		temp = ft_lstnew((void*)new);
 		ft_lstadd_back(&head, temp);
 	}
 	node new = ft_lstmap(head, f, d);
 	while (new){
-		ft_putnbr_fd((int)(new->content), 1);
-		ft_putendl_fd("", 1);
+		ft_putendl_fd((char*)(new->content), 1);
 		new = new->next;
 	}
 	return 0;
